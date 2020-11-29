df14d3b8a327de12a0aba9745bc562009acb6008
Darwin Potes <darwinpotes2102@live.com>
2020-11-29 03:36:20 -0500

    complete the filter for searching

--- Utils/pageAction/wdioAction.js
+++ Utils/pageAction/wdioAction.js
@@ -7,6 +7,22 @@ class wdioAction {
         }
     };
 
+    static isVisible(webElement) {
+        try {
+            return $(webElement).isDisplayed();
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
+
+    static waitForVisibility(webElement) {
+        try {
+            $(webElement).waitForDisplayed(2000);
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
+
     static click(locator){
         try{
             $(locator).click();
